0340b53003fe260aabe70d5c0b8f52650976efa1
Cristina Rivera <cristinapruebas14@gmail.com>
2023-11-20 13:59:17 +0100

    Vistas articulo

---
+++ app/Http/Controllers/ArticuloController.php
@@ -0,0 +1,96 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Articulo;
+use App\Models\Categoria;
+use Illuminate\Http\Request;
+
+class ArticuloController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        return view('articulos.index', [
+            'articulo' => Articulo::all(),
+            'categorias' => Categoria::all(),
+        ]);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        return view('articulos.create', [
+            'categorias' => Categoria::all(),
+        ]);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        $validated = $request->validate([
+            'denominacion' => 'required|max:255',
+            'precio' => 'required|decimal:0,2',
+            'categoria_id' => 'required|exists:categorias,id',
+        ]);
+
+        $articulo = new Articulo();
+        $articulo->denominacion = $request->input('denominacion');
+        $articulo->precio = $request->input('precio');
+        $articulo->categoria_id = $request->input('categoria_id');
+        $articulo->save();
+        return redirect()->route('articulos.index');
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Articulo $articulo)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Articulo $articulo)
+    {
+        return view('articulos.edit', [
+            'articulo' => $articulo,
+            'categorias' => Categoria::all(),
+        ]);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Articulo $articulo)
+    {
+        $validated = $request->validate([
+            'denominacion' => 'required|max:255',
+            'precio' => 'required|decimal:0,2',
+            'categoria_id' => 'required|exists:categorias,id',
+        ]);
+
+        $articulo->denominacion = $request->input('denominacion');
+        $articulo->precio = $request->input('precio');
+        $articulo->categoria_id = $request->input('categoria_id');
+        $articulo->save();
+        return redirect()->route('articulos.index');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Articulo $articulo)
+    {
+        $articulo->delete();
+        return redirect()->route('articulos.index');
+    }
+}
---
+++ resources/views/articulos/edit.blade.php
@@ -0,0 +1,38 @@
+<x-guest-layout>
+    <form method="POST" action="{{ route('articulos.update', ['articulo' => $articulo]) }}">
+        @csrf
+        @method('PUT')
+
+        <!-- Nombre -->
+        <div>
+            <x-input-label for="denominacion" :value="'Nombre del artículo'" />
+            <x-text-input id="denominacion" class="block mt-1 w-full" type="text" name="denominacion" :value="old('denominacion', $articulo->denominacion)" required autofocus autocomplete="denominacion" />
+            <x-input-error :messages="$errors->get('denominacion')" class="mt-2" />
+        </div>
+        <div>
+            <x-input-label for="precio" :value="'Precio'" />
+            <x-text-input id="precio" class="block mt-1 w-full" type="text" name="precio" :value="old('precio', $articulo->precio)" required autofocus autocomplete="precio" />
+            <x-input-error :messages="$errors->get('precio')" class="mt-2" />
+        </div>
+        <div>
+            <x-input-label for="categoria_id" :value="'Categoría'" />
+            <select id="categoria_id" name="categoria_id" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+                @foreach ($categorias as $categoria)
+                <option value="{{$categoria->id}}">{{$categoria->nombre}}</option>
+                @endforeach
+            </select>
+            <x-input-error :messages="$errors->get('categoria_id')" class="mt-2" />
+        </div>
+
+        <div class="flex items-center justify-end mt-4">
+            <a href="{{ route('articulos.index') }}">
+                <x-secondary-button class="ms-4">
+                    Volver
+                </x-primary-button>
+            </a>
+            <x-primary-button class="ms-4">
+                Editar
+            </x-primary-button>
+        </div>
+    </form>
+</x-guest-layout>
